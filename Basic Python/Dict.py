e401a5066115374ebad440f2294c0b65ecc31ab5
Betsabeh Tanoori <46868386+Betsabeh@users.noreply.github.com>
2024-05-08 20:30:18 +0330

    Update Dict.py

--- Basic Python/Dict.py
+++ Basic Python/Dict.py
@@ -18,4 +18,18 @@ Dic['name'] = 'Mary'
 # 5-convert to list
 list_Dic = list (Dic.items())
 print(list_Dic)
+print(list_Dic[2]) # print list index=2
+Dic = dic(list_Dic) #convert back to the Dictionary
+
+# 6-Merge 2 Dic
+def Merge_Dic(D1,D2):
+  merged_dict = D1.copy()
+  return (merged_dict.update(D2))
+
+D1={"a":2,"B":3}
+D2={"g":5, "3":4 , "r":5}
+Merge_Dic(D1,D2)
+print(D1)  
+
+
 
